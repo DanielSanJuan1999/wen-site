d3ba13243820721289dbf9ef8f906d9ab545a38d
DanielSanJuan <dan.sanjuan@alumnos.duoc.cl>
2021-03-17 16:54:13 -0300

    Add contact form
    Se añade (enmaquetado) formulario de contacto

--- js/app.js
+++ js/app.js
@@ -4,3 +4,19 @@ setTimeout(function() {
     .removeClass('loading')
     .addClass('loaded');
 }, 2438);
+
+//FORMULARIO:
+
+//  Variables
+
+eventListener();
+
+//  Funciones
+
+function eventListener(){
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+}
+
+function iniciarApp(){
+    console.log('Iniciando...');
+}
